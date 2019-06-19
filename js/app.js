1d067e273fe5351a429d4fc58f57027e55496009
ingilion <47861284+ingilion@users.noreply.github.com>
2019-06-19 20:05:58 +0500

    Upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var box = document.getElementById("box");
+generate(100);
+
+function generate(len) {
+    for (var i = 1; i <= len; i++) {
+        if ((i % 2 != 0 || i == 2) && (i % 3 != 0 || i == 3) && (i % 5 != 0 || i == 5) && (i % 7 != 0 || i == 7)) {
+            box.innerHTML += "<div class=\"number prime\">" + i + "</div>";
+        } else {
+            box.innerHTML += "<div class=\"number\">" + i + "</td>";
+        }
+    }
+}
+
+function setvalue() {
+    box.innerHTML = "";
+    this.dataset.length = this.value;
+    generate(this.value);
+};
+
+document.getElementById("length").oninput = function () {
+    this.dataset.length = this.value;
+};
+
+document.getElementById("length").ontouchend = setvalue;
+document.getElementById("length").onmouseup = setvalue;
